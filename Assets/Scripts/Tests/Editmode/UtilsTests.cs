1a40c4414dc673dcd58ad0efd9a897360034be21
stuart-payne <paynestu@gmail.com>
2021-10-24 23:52:47 +0100

    Ticker Tests + more Util Tests

--- Assets/Scripts/Tests/Editmode/UtilsTests.cs
+++ Assets/Scripts/Tests/Editmode/UtilsTests.cs
@@ -1,17 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using NUnit.Framework;
 using Snek.Core;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace Snek
+namespace Snek.Tests
 {
     public class UtilsTests
     {
-        // A Test behaves as an ordinary method
         [Test]
-        public void IsOppositeDirection()
+        public void IsOppositeDirectionTests()
         {
             var testCases = new[]
             {
@@ -26,6 +24,34 @@ namespace Snek
             foreach (var (dir1, dir2, expected) in testCases)
             {
                 Assert.AreEqual(Utils.IsOppositeDirection(dir1, dir2), expected);
+            }
+        }
+
+        [Test]
+        public void DirectionToVector2IntTests()
+        {
+            var testCases = new[]
+            {
+                (Direction.Up, new Vector2Int(0, 1)),
+                (Direction.Down, new Vector2Int(0, -1)),
+                (Direction.Left, new Vector2Int(-1, 0)),
+                (Direction.Right, new Vector2Int(1, 0))
+            };
+
+            foreach (var (direction, expectedVector) in testCases)
+            {
+                Assert.AreEqual(Utils.DirectionToVector2Int(direction), expectedVector);
+            }
+        }
+
+        
+        [Test]
+        public void GetRandomDirectionTests()
+        {
+            const int testIterations = 100;
+            for (var i = 0; i < testIterations; i++)
+            {
+                Assert.IsTrue(Enum.IsDefined(typeof(Direction), Utils.GetRandomDirection()));
             }
         }
     }
---
+++ Assets/Scripts/Tests/Playmode/TickerTests.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Snek.Core;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Snek.Tests
+{
+    public class TickerTests
+    {
+
+        [UnityTest]
+        public IEnumerator WillTickOnceAfterTickRatePassed()
+        {
+            var ticker = SetupTicker(1.0f);
+            var testTickable = new TestTickable();
+            ticker.AddTickable(testTickable);
+            ticker.StartTick();
+            yield return new WaitForSeconds(1.1f);
+            Assert.IsTrue(testTickable.TickCount == 1);
+        }
+
+        [UnityTest]
+        public IEnumerator WIllTickOnceThenStopTickingAfter()
+        {
+            var ticker = SetupTicker(0.2f);
+            var testTickable = new TestTickable();
+            ticker.AddTickable(testTickable);
+            ticker.StartTick();
+            yield return new WaitForSeconds(0.3f);
+            Assert.IsTrue(testTickable.TickCount == 1);
+            ticker.StopTick();
+            yield return new WaitForSeconds(0.3f);
+            Assert.IsTrue(testTickable.TickCount == 1);
+        }
+
+        [UnityTest]
+        public IEnumerator WillStopTickingTickableAfterRemoval()
+        {
+            var ticker = SetupTicker(0.2f);
+            var tickableToStay = new TestTickable();
+            var tickableToBeRemoved = new TestTickable();
+            
+            ticker.AddTickable(tickableToStay);
+            ticker.AddTickable(tickableToBeRemoved);
+            ticker.StartTick();
+
+            yield return new WaitForSeconds(0.3f);
+            ticker.RemoveTickable(tickableToBeRemoved);
+            yield return new WaitForSeconds(0.3f);
+            
+            Assert.IsTrue(tickableToStay.TickCount == 2);
+            Assert.IsTrue(tickableToBeRemoved.TickCount == 1);
+        }
+
+        private class TestTickable : ITickable
+        {
+            public int TickCount = 0;
+            public void Tick() => TickCount++;
+        }
+
+        private Ticker SetupTicker(float tickRate)
+        {
+            var tickerObj = new GameObject();
+            var ticker = tickerObj.AddComponent<Ticker>();
+            ticker.TickRate = tickRate;
+            return ticker;
+        }
+    }
+}
